67af8efc73a88731c6c13fed3149c23bf9cb4895
sm-123 <sourav16021998@gmail.com>
2021-10-01 07:15:19 -0700

    taking input and console

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var username= prompt("Give me your username")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
-var welcomeMessage="This script works!! " + username;
+console.log(txtInput)
 
-alert(welcomeMessage);
+function clickHandler() {
+    console.log("clicked!");
+    console.log("input", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler)
